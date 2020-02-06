a4ae0b565810fc0a0e69a85e1ac01f1613c47615
RAVI SINGH <43233175+ravising-h@users.noreply.github.com>
2020-02-06 22:42:23 +0530

    Update faster_rcnn_r101_fpn_1x.py

--- configs/faster_rcnn_r101_fpn_1x.py
+++ configs/faster_rcnn_r101_fpn_1x.py
@@ -168,7 +168,7 @@ log_config = dict(
 total_epochs = 5
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = '/content/work_dirs1/faster_rcnn_r101_fpn_1x'
+work_dir = '/content/work_dirs2/faster_rcnn_r101_fpn_1x'
 load_from = None
 resume_from = None # "/content/drive/My Drive/DL/epoch_8.pth"
 workflow = [('train', 1)]
