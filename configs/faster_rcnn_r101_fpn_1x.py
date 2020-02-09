3e45d1b4f67356180d14d2507ff4793450c4d7c6
RAVI SINGH <43233175+ravising-h@users.noreply.github.com>
2020-02-09 20:00:24 +0530

    Update faster_rcnn_r101_fpn_1x.py

--- configs/faster_rcnn_r101_fpn_1x.py
+++ configs/faster_rcnn_r101_fpn_1x.py
@@ -142,8 +142,8 @@ data = dict(
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
-        ann_file="/content/test.txt",
-        img_prefix="/content/RFC_data" ,
+        ann_file= data_root + "test.txt",
+        img_prefix=data_root ,
         pipeline=test_pipeline))
 # optimizer
 optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
